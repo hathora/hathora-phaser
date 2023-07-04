931f2a7ac466e315057422a3460319b49154bbe1
Kirk Morris <worker.kirk@gmail.com>
2023-07-04 15:07:33 -0700

    Implements outstanding UI components.

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,11 @@
 import { Plugins, Scene } from "phaser";
-import { AuthV1Api, LobbyV2Api, DiscoveryV1Api, DiscoveryResponseInner, RoomV1Api } from "@hathora/hathora-cloud-sdk";
-import { HathoraConnection } from "@hathora/client-sdk";
+import { AuthV1Api, LobbyV2Api, DiscoveryV1Api, DiscoveryResponseInner, RoomV1Api, Region } from "@hathora/hathora-cloud-sdk";
+import { ConnectionInfo, HathoraConnection } from "@hathora/client-sdk";
 import { v4 as uuid } from 'uuid';
 // @ts-ignore
 import css from './styles.css?raw';
 
+const STORAGE_KEY_TOKEN = 'hathora-phaser3:token';
 const STORAGE_KEY_CREATING_PUBLIC_GAME = 'hathora-phaser3:creating_public_game';
 const STORAGE_KEY_DEFAULT_REGION = 'hathora-phaser3:default_region';
 
@@ -17,31 +18,16 @@ class HathoraPhaser extends Plugins.ScenePlugin {
 
   // Create game configuration
   public isCreatingPublicGame: boolean = (['true', null].includes(window.localStorage.getItem(STORAGE_KEY_CREATING_PUBLIC_GAME)));
-  public selectedRegion: string = (window.localStorage.getItem(STORAGE_KEY_DEFAULT_REGION) || 'Seattle');
+  public selectedRegion: Region = (window.localStorage.getItem(STORAGE_KEY_DEFAULT_REGION) as Region || 'Seattle');
   public onRegionsLoad: Promise<void>[] = [];
+
+  // CB functions for joining a room
   public onJoin?: Function;
+  public onError?: Function;
   
   // Current game's appId
   private appId!: string;
   private token!: string;
-  // List of all available server regions
-  // private regions: string[] = [
-  //   'Seattle',
-  //   'Washington_DC',
-  //   'Chicago',
-  //   'London',
-  //   'Frankfurt',
-  //   'Mumbai',
-  //   'Singapore',
-  //   'Tokyo',
-  //   'Sydney',
-  //   'Sao_Paulo'
-  // ];
-  // Currently selected region, defaults to Seattle
-  // private region: Region = 'Seattle';
-
-  // DOM node id tracking
-  // private domPublicLobbyListIDs: string[] = [];
 
   constructor(scene: Scene, manager: Plugins.PluginManager) {
     super(scene, manager, 'HathoraPhaser');
@@ -63,32 +49,61 @@ class HathoraPhaser extends Plugins.ScenePlugin {
     console.log(this.authClient);
     console.log(this.lobbyClient);
     console.log(this.discoveryClient);
-    console.log(this.token);
   }
 
-  public async startUI(appId: string, onJoin: Function, onError: Function, useUrl: boolean = true) {
+  public async initialize(appId: string, onJoin: Function, onError: Function, useUrl: boolean = true) {
     if (this.appId) {
-      console.error('You should only call startUI once.');
+      console.error('You should only call initialize once.');
       return;
     }
 
-    const { token } = await this.authClient.loginAnonymous(appId);
-    
     this.appId = appId;
-    this.token = token;
     this.onJoin = onJoin;
+    this.onError = onError;
+
+    let token: any = '';
+
+    if (sessionStorage.getItem(STORAGE_KEY_TOKEN)) {
+      token = sessionStorage.getItem(STORAGE_KEY_TOKEN)!;
+    }
+
+    if (token === '') {
+      const authData = await this.authClient.loginAnonymous(appId);
+      token = authData.token;
+
+      sessionStorage.setItem(STORAGE_KEY_TOKEN, token);
+    }
+
+    this.token = token;
 
     const {pathname} = window.location;
     const roomId = pathname.substring(1);
+    const {hostname} = window.location;
+    const isLocal = (hostname === 'localhost');
+    let connectionInfo: ConnectionInfo;
 
     if (useUrl && roomId !== '') {
       try {
-        const connectionInfo = await this.roomClient.getConnectionInfo(
-          appId,
-          roomId
-        );
+        if (isLocal) {
+          connectionInfo = {
+            host: 'localhost',
+            port: 4000,
+            transportType: 'tcp',
+            status: 'active'
+          };
+        }
+        else {
+          connectionInfo = await this.roomClient.getConnectionInfo(
+            appId,
+            roomId
+          );
+        }
+
+        const connection = new HathoraConnection(roomId, connectionInfo);
+
+        connection.connect(token);
         
-        onJoin(new HathoraConnection(roomId, connectionInfo));
+        onJoin(connection);
       }
       catch (e) {
         onError(e);
@@ -104,7 +119,7 @@ class HathoraPhaser extends Plugins.ScenePlugin {
     const privateId = `private_${uuid()}`;
 
 
-    this.scene.add.dom(x, y).createFromHTML(`
+    const ele = this.scene.add.dom(x, y).createFromHTML(`
       <div class="ha-toggle">
         <button id="${publicId}" class="ha-toggle__switch${publicGame ? ' on': ''}">
           Public
@@ -136,7 +151,7 @@ class HathoraPhaser extends Plugins.ScenePlugin {
       HathoraPhaser.isCreatingPublicGame = false;
     });
 
-    return [btnPrivate, btnPublic];
+    return ele;
   }
 
   private factoryRegionSelect(x: number, y: number) {
@@ -144,7 +159,7 @@ class HathoraPhaser extends Plugins.ScenePlugin {
     const {HathoraPhaser} = this.scene;
     const selectId = `region_${uuid()}`;
 
-    this.scene.add.dom(x, y).createFromHTML(`
+    const ele = this.scene.add.dom(x, y).createFromHTML(`
       <select id="${selectId}" class="ha-select">
         <option value="Seattle">Seattle</option>
         <option value="Washington_DC">Washington, DC</option>
@@ -210,15 +225,15 @@ class HathoraPhaser extends Plugins.ScenePlugin {
       HathoraPhaser.selectedRegion = region;
     });
 
-    return select;
+    return ele;
   }
 
   private factoryCreateGameButton(x: number, y: number, label: string = 'Create Game') {
     // @ts-ignore
-    const {HathoraPhaser} = this.scene;
+    const {HathoraPhaser}: { HathoraPhaser: HathoraPhaser } = this.scene;
     const btnId = `create_${uuid()}`;
 
-    this.scene.add.dom(x, y).createFromHTML(`
+    const ele = this.scene.add.dom(x, y).createFromHTML(`
       <button id="${btnId}" class="ha-btn">
         ${label}
       </button>
@@ -235,38 +250,62 @@ class HathoraPhaser extends Plugins.ScenePlugin {
     btn.addEventListener('click', async () => {
       const {hostname} = window.location;
       const isLocal = (hostname === 'localhost');
+      const visibility = (isLocal ? 'local' : HathoraPhaser.isCreatingPublicGame ? 'public' : 'private');
+
+      console.log(isLocal);
+      console.log(visibility);
 
       const lobby = await HathoraPhaser.lobbyClient.createLobby(
         HathoraPhaser.appId,
         HathoraPhaser.token,
         {
-          visibility: isLocal ? 'local' : HathoraPhaser.isCreatingPublicGame ? 'public' : 'private',
+          visibility,
           region: HathoraPhaser.selectedRegion,
           initialConfig: {}
         }
       );
 
+      let connectionInfo: ConnectionInfo;
       const {roomId} = lobby;
 
-      console.log(roomId);
-      console.log(isLocal ? 'local' : HathoraPhaser.isCreatingPublicGame ? 'public' : 'private');
-      
-      // const connectionInfo = await HathoraPhaser.roomClient.getConnectionInfo(
-      //   HathoraPhaser.appId,
-      //   roomId
-      // );
-      
-      // HathoraPhaser.onJoin(connectionInfo);
+      try {
+        if (visibility === 'local') {
+          connectionInfo = {
+            host: 'localhost',
+            port: 4000,
+            transportType: 'tcp',
+            status: 'active'
+          };
+        }
+        else {
+          connectionInfo = await HathoraPhaser.roomClient.getConnectionInfo(
+            HathoraPhaser.appId,
+            roomId
+          );
+        }
+  
+        const connection = new HathoraConnection(roomId, connectionInfo);
+  
+        connection.connect(HathoraPhaser.token);
+        
+        HathoraPhaser.onJoin!(connection);
+        history.pushState({}, "", `/${roomId}`);
+      }
+      catch (e) {
+        HathoraPhaser.onError!(e);
+      }
     });
 
-    return btn;
+    return ele;
   }
 
   private factoryJoinPrivateInput(x: number, y: number, label: string = 'Join') {
+    // @ts-ignore
+    const {HathoraPhaser}: {HathoraPhaser: HathoraPhaser} = this.scene;
     const txtId = `join_private_${uuid()}`;
     const btnId = `join_private_${uuid()}`;
 
-    this.scene.add.dom(x, y).createFromHTML(`
+    const ele = this.scene.add.dom(x, y).createFromHTML(`
       <div class="ha-join-private">
         <input id="${txtId}" class="ha-text ha-text--join" />
         <button id="${btnId}" class="ha-btn ha-btn--join">
@@ -278,20 +317,49 @@ class HathoraPhaser extends Plugins.ScenePlugin {
     const btn = document.getElementById(btnId)! as HTMLButtonElement;
     const txt = document.getElementById(txtId)! as HTMLInputElement;
 
-    btn.addEventListener('click', () => {
-      // alert(select.value);
-      alert(txt.value);
+    btn.addEventListener('click', async () => {
+      const roomId = txt.value.trim();
+      const {hostname} = window.location;
+      const isLocal = (hostname === 'localhost');
+      let connectionInfo: ConnectionInfo;
+
+      try {
+        if (isLocal) {
+          connectionInfo = {
+            host: 'localhost',
+            port: 4000,
+            transportType: 'tcp',
+            status: 'active'
+          };
+        }
+        else {
+          connectionInfo = await HathoraPhaser.roomClient.getConnectionInfo(
+            HathoraPhaser.appId,
+            roomId
+          );
+        }
+
+        const connection = new HathoraConnection(roomId, connectionInfo);
+
+        connection.connect(HathoraPhaser.token);
+        
+        HathoraPhaser.onJoin!(connection);
+        history.pushState({}, "", `/${roomId}`);
+      }
+      catch (e) {
+        HathoraPhaser.onError!(e);
+      }
     });
 
-    return [btn, txt];
+    return ele;
   }
 
-  private factoryJoinPublicList(x: number, y: number, width: number = 200, height: number = 350, label: string = 'Join Public Game') {
+  private factoryJoinPublicList(x: number, y: number, width: number = 200, height: number = 350, label: string = 'Join Public Game', pollRate: number = 1000) {
+    // @ts-ignore
+    const {HathoraPhaser}: {HathoraPhaser: HathoraPhaser} = this.scene;
     const listId = `public_list_${uuid()}`;
 
-    // this.domPublicLobbyListIDs.push(listId);
-
-    return this.scene.add.dom(x, y).createFromHTML(`
+    const ele = this.scene.add.dom(x, y).createFromHTML(`
       <div class="ha-join-public" style="width: ${width}px; height: ${height}px;">
         <header class="ha-join-public__header">
           ${label}
@@ -301,18 +369,80 @@ class HathoraPhaser extends Plugins.ScenePlugin {
           <div class="ha-loader"></div>
       </div>
     `).setOrigin(0.5);
+
+    const list = document.getElementById(listId);
+
+    const joinPublicGame = async (e: any) => {
+      const roomId = e.target.getAttribute('data-join-room-id');
+      const {hostname} = window.location;
+      const isLocal = (hostname === 'localhost');
+      let connectionInfo: ConnectionInfo;
+
+      try {
+        if (isLocal) {
+          connectionInfo = {
+            host: 'localhost',
+            port: 4000,
+            transportType: 'tcp',
+            status: 'active'
+          };
+        }
+        else {
+          connectionInfo = await HathoraPhaser.roomClient.getConnectionInfo(
+            HathoraPhaser.appId,
+            roomId
+          );
+        }
+
+        const connection = new HathoraConnection(roomId, connectionInfo);
+
+        connection.connect(HathoraPhaser.token);
+        
+        HathoraPhaser.onJoin!(connection);
+        history.pushState({}, "", `/${roomId}`);
+      }
+      catch (e) {
+        HathoraPhaser.onError!(e);
+      }
+    };
+
+    const pollPublicLobbies = async () => {
+      const publicLobbies = await HathoraPhaser.lobbyClient.listActivePublicLobbies(HathoraPhaser.appId, HathoraPhaser.selectedRegion);
+      let listHTML = ``;
+
+      if (publicLobbies.length > 0) {
+        publicLobbies.forEach((lobby) => {
+          listHTML += `
+            <div class="ha-join-public__game">
+              <label>${lobby.roomId}</label>
+              <button data-join-room-id="${lobby.roomId}" class="ha-btn ha-btn--join">
+                Join
+              </button>
+            </div>
+          `;
+        });
+        list!.classList.add('ha-join-public__list--filled');
+      }
+      else {
+        listHTML = `<div class="ha-join-public__nogames">No active games in ${HathoraPhaser.selectedRegion} region.</div>`;
+        list!.classList.remove('ha-join-public__list--filled');
+      }
+
+      list!.innerHTML = listHTML;
+
+      list!.querySelectorAll('[data-join-room-id]').forEach((btn) => {
+        btn.addEventListener('click', joinPublicGame);
+      });
+    };
+
+    setInterval(pollPublicLobbies, pollRate);
+    pollPublicLobbies();
+
+    return ele;
   }
 
   // private async refreshPublicLobbyLists() {
   //   // Show spinners on all lobby lists
-  //   this.domPublicLobbyListIDs.forEach((id) => {
-  //     const lobbyListDiv = document.getElementById(id);
-      
-  //     if (lobbyListDiv) {
-  //       lobbyListDiv.innerHTML = `<div class="ha-loader"></div>`;
-  //     }
-  //   });
-
   //   const publicLobbies = await this.lobbyClient.listActivePublicLobbies(this.appId!, this.region);
 
   //   this.domPublicLobbyListIDs.forEach((id) => {
